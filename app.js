92c8d3e9ed3687312cd2e98719b492058cff50f7
tulsee <ramtulsi022@gmail.com>
2019-02-21 02:15:21 +0545

    routes and config added

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
-//load user Models
+//load  Models
 require('./models/user')
+require('./models/Story');
 
 // passport config
 require('./config/passport')(passport);
@@ -16,9 +20,19 @@ const app = express();
 //load routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
 //load keys
 const keys = require('./config/keys');
+
+//handlebars helper
+const {
+    truncate,
+    stripTags,
+    formatDate, 
+    select,
+    editIcon
+} = require('./helpers/hbs');
 
 //map global promise
 mongoose.Promise = global.Promise;
@@ -28,9 +42,25 @@ mongoose.connect(keys.mongoURI)
     .then(() => console.log('mongodb connected'))
     .catch(err => console.log('err'));
 
+//middle ware for body- parser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+//middleware for method override
+app.use(methodOverride('_method')); 
+
 
 //middleware for handlebars
 app.engine('handlebars', exphbs({
+    helpers:{
+        truncate: truncate,
+        stripTags: stripTags,
+        formatDate:formatDate,
+        select: select,
+        editIcon:editIcon
+    },
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
@@ -53,9 +83,13 @@ app.use((req, res, next) => {
     next();
 });
 
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //use routes
-app.use('/', index); 
+app.use('/', index);
 app.use('/auth', auth);
+app.use('/stories', stories);
 
 
 
